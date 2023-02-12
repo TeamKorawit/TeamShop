05391dd989974b4862c836d58f99a239b11e03a5
TeamKorawit <korawit4831@gmail.com>
2023-02-12 16:55:49 +0700

    first commit

---
+++ lib/model/modelItem.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class itemModel {
+  itemModel({
+    this.idItem,
+    this.nameItem,
+    this.priceItem,
+    this.discountItem,
+    this.starItem,
+    this.dataItem,
+    this.imagePath,
+  });
+
+  int? idItem;
+  String? nameItem;
+  String? discountItem;
+  num? priceItem;
+  int? starItem;
+  TextEditingController? dataItem;
+  dynamic imagePath;
+}
